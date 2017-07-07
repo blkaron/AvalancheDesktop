efb34a47e245c73d29210cb6e6dd76afeca3bc22
Viktor Stoev <v.stoev@prosfit.com>
2017-07-07 17:46:59 +0300

    Improving layout

--- AvalancheDesktop/main.py
+++ AvalancheDesktop/main.py
@@ -3,13 +3,15 @@ import sys
 
 from resources import pyqtgraph as pg
 from resources.pyqtgraph.Qt import QtWidgets, QtCore, QtGui
+from resources.ui.mainWindow import Ui_MainWindow
 import numpy as np
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
 
+        self.setupUi(self)
         self.initUI()
 
     def initUI(self):
@@ -37,32 +39,59 @@ class MainWindow(QtWidgets.QMainWindow):
     def newFile():
         pass
 
-    def Open():
-        pass
+    def open(self):
+        if self.maybeSave():
+            fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self)
+            if fileName:
+                self.loadFile(fileName)
 
-    def Save():
+    def save():
         pass
 
     def saveAs():
         pass
 
     def createActions(self):
-        self.exitAct = QtWidgets.QAction("E&xit", self, shortcut="Ctrl+Q",
+        self.exitAct = QtWidgets.QAction("&Exit", self, shortcut="Ctrl+Q",
                                statusTip="Exit the application", triggered=self.close)
         self.newAct = QtWidgets.QAction("&New", self, shortcut=QtGui.QKeySequence.New,
                                statusTip="Create a new file", triggered=self.newFile)
         self.openAct = QtWidgets.QAction("&Open...", self, shortcut=QtGui.QKeySequence.Open,
-                               statusTip="Open an existing file", triggered=self.Open)
+                               statusTip="Open an existing file", triggered=self.open)
         self.saveAct = QtWidgets.QAction("&Save", self, shortcut=QtGui.QKeySequence.Save,
-                               statusTip="Save the document to disk", triggered=self.Save)
+                               statusTip="Save the document to disk", triggered=self.save)
         self.saveAsAct = QtWidgets.QAction("Save &As...", self, shortcut=QtGui.QKeySequence.SaveAs,
                                statusTip="Save the document under a new name", triggered=self.saveAs)
         self.aboutQtAct = QtWidgets.QAction("About &Qt", self, statusTip="Show the Qt library's About box",
                                triggered=QtGui.QApplication.instance().aboutQt)
+
+    def maybeSave(self):
+        if self.textEdit.document().isModified():
+            ret = QtWidgets.QMessageBox.warning(self, "Application",
+                    "The document has been modified.\nDo you want to save "
+                    "your changes?",
+                    QtWidgets.QMessageBox.Save | QtWidgets.QMessageBox.Discard | QtWidgets.QMessageBox.Cancel)
+
+            if ret == QtWidgets.QMessageBox.Save:
+                return self.save()
+
+            if ret == QtWidgets.QMessageBox.Cancel:
+                return False
+
+        return True
 
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     avalancheDesktop = MainWindow()
     avalancheDesktop.show()
+
+    #cw = QtGui.QWidget()
+    #avalancheDesktop.setCentralWidget(cw)
+    #cw2 = QtGui.QWidget()
+    #l = QtGui.QHBoxLayout()
+    #cw.setLayout(l)
+
+    #pw = pg.PlotWidget(name='Plot1')  ## giving the plots names allows us to link their axes together
+    #l.addWidget(pw)
     sys.exit(app.exec_())
---
+++ AvalancheDesktop/resources/ui/mainWindow.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.7.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 161, 531))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton_4 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_4.setEnabled(True)
+        self.pushButton_4.setMinimumSize(QtCore.QSize(0, 44))
+        self.pushButton_4.setDefault(False)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.verticalLayout.addWidget(self.pushButton_4)
+        self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setMinimumSize(QtCore.QSize(0, 44))
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        spacerItem = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(180, 10, 611, 531))
+        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 19))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton_4.setToolTip(_translate("MainWindow", "Open file for visualization"))
+        self.pushButton_4.setText(_translate("MainWindow", "Open File"))
+        self.pushButton.setToolTip(_translate("MainWindow", "Open serial connection for real time visualization"))
+        self.pushButton.setText(_translate("MainWindow", "Open Serial Connection"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
